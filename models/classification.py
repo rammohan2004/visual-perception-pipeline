c466d4773c75773884f28a1fb61d2f2b32b08da6
Ram mohan <ramamohanraobotla12@gmail.com>
2026-04-06 15:42:46 +0530

    Implemented classification

--- models/classification.py
+++ models/classification.py
@@ -3,12 +3,14 @@
 
 import torch
 import torch.nn as nn
+from .vgg11 import VGG11Encoder
+from .layers import CustomDropout
 
 
 class VGG11Classifier(nn.Module):
     """Full classifier = VGG11Encoder + ClassificationHead."""
 
-    def __init__(self, num_classes: int = 37, in_channels: int = 3, dropout_p: float = 0.5):
+    def __init__(self, num_classes: int = 37, in_channels: int = 3, dropout_p: float = 0.5,  use_batchnorm: bool = True):
         """
         Initialize the VGG11Classifier model.
         Args:
@@ -16,7 +18,19 @@ class VGG11Classifier(nn.Module):
             in_channels: Number of input channels.
             dropout_p: Dropout probability for the classifier head.
         """
-        pass
+        super().__init__()
+        self.encoder =VGG11Encoder(in_channels=in_channels, use_batchnorm=use_batchnorm)
+        self.head =nn.Sequential(
+            nn.Linear(512*7*7,4096),
+            nn.ReLU(inplace=True),
+            CustomDropout(p=dropout_p),
+
+            nn.Linear(4096,4096),
+            nn.ReLU(inplace=True),
+            CustomDropout(p=dropout_p),
+
+            nn.Linear(4096, num_classes)
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Forward pass for classification model.
@@ -26,4 +40,14 @@ class VGG11Classifier(nn.Module):
             Classification logits [B, num_classes].
         """
         # TODO: Implement forward pass.
-        raise NotImplementedError("Implement VGG11Classifier.forward")
+        #encoder
+        x = self.encoder(x)
+        
+        #flatten
+        x = torch.flatten(x, start_dim=1)
+        
+        #head
+        x = self.head(x)
+        
+        #Return logits
+        return x
